7248db965c3d9d28a4e1b6284427a0d917a737ce
Harry Bloch <harryfbloch@gmail.com>
2018-09-20 21:18:22 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -77,7 +77,7 @@ end
 def organize_schools(collection)
    by_location = {}
     collection.each do |school, location_hash|
-      puts school
+      puts location
       location_hash.each do |symbol, location|
         if by_location[location] == nil
           by_location[location] = [school]
